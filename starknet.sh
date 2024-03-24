7ff731891c9ae3e6fc30ace9ce76bf1da8fd8a8f
Dante4rt <rxmxdhxni@gmail.com>
2024-03-24 14:27:16 +0700

    fix: starknet url

--- starknet.sh
+++ starknet.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Perform POST request to fetch StarkNet data
-starknet_data=$(curl -s -H "Content-Type: application/json" -X POST --data '{"jsonrpc":"2.0","id":"1","method":"starknet_blockNumber"}' https://your-starknet-rpc-url.com)
+starknet_data=$(curl -s -H "Content-Type: application/json" -X POST --data '{"jsonrpc":"2.0","id":"1","method":"starknet_blockNumber"}' https://rpc.starknet.lava.build/lava-referer-8b51600b-b3cc-4893-89ec-5de41f0724db/)
 
 # Append the output to a file named starknet.txt
 echo "$starknet_data" >> starknet.txt
