753daadea18341e839ecbf458cc547d0d8d09649
Dante4rt <rxmxdhxni@gmail.com>
2024-03-24 13:56:49 +0700

    fix: runner again v2

--- ethereum.sh
+++ ethereum.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Perform POST request to fetch Ethereum data
-eth_data=$(curl -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_gasPrice","params":["latest", false],"id":1}' https://eth1.lava.build/lava-referer-8b51600b-b3cc-4893-89ec-5de41f0724db/)
+eth_data=$(curl -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}' https://eth1.lava.build/lava-referer-8b51600b-b3cc-4893-89ec-5de41f0724db/)
 
 # Append the output to a file named ethereum.txt
 echo "$eth_data" >> ethereum.txt
